b1927373aef77488f5ad1a108d110249850c116a
Divya2398 <divya.platosys@gmail.com>
2022-09-07 19:40:51 +0530

    'weather'

---
+++ src/components/weathertest.js
@@ -0,0 +1,43 @@
+// import "./App.css";
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
+// import moment from "moment";
+// import { Weather } from "./components/Weather";
+
+// export default function App() {
+//   const API_URL = "https://api.openweathermap.org/data/2.5";
+//   const API_KEY = "5a041aaf0eb3429d9e5d42a5964be14c";
+//   const REACT_APP_ICON_URL = "https://openweathermap.org/img/w";
+//   const [lat, setLat] = useState([]);
+//   const [long, setLong] = useState([]);
+//   const [data, setData] = useState([]);
+
+//   useEffect(() => {
+//     const weatherData = async () => {
+//       navigator.geolocation.getCurrentPosition(function (position) {
+//         setLat(position.coords.latitude);
+//         setLong(position.coords.longitude);
+//       });
+//       console.log("lat", lat);
+//       await fetch(
+//         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=5a041aaf0eb3429d9e5d42a5964be14c`
+//       )
+//         .then((res) => res.json())
+//         .then((result) => {
+//           setData(result);
+//           console.log(result);
+//         });
+//     };
+//     weatherData();
+//   }, [lat, long]);
+
+//   return (
+//     <div className="App">
+//       {typeof data.main != "undefined" ? (
+//         <Weather weatherData={data} />
+//       ) : (
+//         <div></div>
+//       )}
+//     </div>
+//   );
+// }
